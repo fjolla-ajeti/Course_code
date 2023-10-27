f043aba5c1bef1f951491fefee252a0f3280a714
fjolla-ajeti <fjollaajeti9@gmail.com>
2023-10-27 21:44:54 +0200

    added-module1

---
+++ 8.Modules and standard library/module1.py
@@ -0,0 +1,7 @@
+#Imports practice
+
+#Make a directory with 2 modules; make a function in one of them; then import this function in the other module and use that in your script of choice.
+
+
+def writeName(name):
+    return f"Hello, {name}!"
